871ba5720a958323052c855a84b6d5f5d5fb9133
Jack-Ricketts <zygga.k@gmail.com>
2022-04-04 21:03:32 +0300

    composition

--- lineTask/app.js
+++ lineTask/app.js
@@ -1,5 +1,48 @@
 "use strict";
 class Point {
+    constructor(_x, _y) {
+        this._x = _x;
+        this._y = _y;
+    }
+    get x() {
+        return this._x;
+    }
+    get y() {
+        return this._y;
+    }
+    set x(x) {
+        this._x = x;
+    }
+    set y(y) {
+        this._y = y;
+    }
+    distanceFromOrigin() {
+        return Math.sqrt(this._x ** 2 + this._y ** 2);
+    }
+    translate(x, y) {
+        this._x += x;
+        this._y += y;
+    }
+    toString() {
+        return '[' + this._x + ';' + this._y + ']';
+    }
+    distance(p) {
+        return Math.sqrt((p.x - this._x) ** 2 + (p.y - this._y) ** 2);
+        // 8.12 this.x yra p1
+    }
+}
+const p1 = new Point(9, 5);
+const p2 = new Point(0, 8);
+const t1 = new Point(0, 8);
+t1.translate(0, 5);
+p1.toString();
+console.log(p1.distanceFromOrigin());
+p2.toString();
+console.log(p2.distanceFromOrigin());
+t1.toString();
+console.log(t1.distanceFromOrigin());
+console.log(p1.distance(p2));
+class Line {
     constructor(_p1, _p2) {
         this._p1 = _p1;
         this._p2 = _p2;
@@ -16,14 +59,15 @@ class Point {
     set p2(p2) {
         this._p2 = p2;
     }
-    toString(p) {
-        return '[' + this._p1 + ';' + this._p2 + ']' + '[' + p.p1 + ';' + p.p2 + ']';
+    toString() {
+        return this._p1.toString() + '-' + this._p2.toString();
     }
-    distance(p) {
-        return Math.sqrt((p.p1 - this._p1) ** 2 + (p.p2 - this._p2) ** 2);
+    length() {
+        return this._p1.distance(p2);
     }
 }
-const coord1 = new Point(1, 1);
-const coord2 = new Point(9, 1);
-console.log('Taskai yra: ' + coord1.toString(coord2));
-console.log(coord1.distance(coord2));
+let taskas1 = new Point(1, 1);
+let taskas2 = new Point(9, 1);
+let taskai = new Line(taskas1, taskas2);
+console.log(taskai.toString());
+console.log(taskai.length());
--- lineTask/app.ts
+++ lineTask/app.ts
@@ -1,7 +1,70 @@
 class Point{
     constructor (
-        private _p1:number,
-        private _p2:number
+        private _x:number,
+        private _y:number
+    ){
+        
+    }
+
+    get x(){
+        return this._x;
+    }
+
+    get y(){
+        return this._y;
+    }
+
+    set x(x:number){
+        this._x=x;
+    }
+
+    set y(y:number){
+        this._y=y;
+    }
+
+    public distanceFromOrigin():number{
+        return Math.sqrt(this._x**2+this._y**2);
+    }
+
+    public translate(x:number, y:number){
+        this._x+=x;
+        this._y+=y;
+    }
+
+    public toString(){
+        return '['+this._x+';'+this._y+']';
+    }
+
+    public distance(p:Point){
+        return Math.sqrt((p.x - this._x)**2 + (p.y - this._y)**2);
+        // 8.12 this.x yra p1
+    }
+    
+}
+
+const p1=new Point(9,5);
+const p2=new Point(0,8);
+
+const t1=new Point(0,8);
+t1.translate(0,5);
+
+p1.toString();
+console.log(p1.distanceFromOrigin());
+
+p2.toString();
+console.log(p2.distanceFromOrigin());
+
+t1.toString();
+console.log(t1.distanceFromOrigin());
+
+console.log(p1.distance(p2));
+
+
+
+class Line{
+    constructor (
+        private _p1:Point,
+        private _p2:Point
     ){
         
     }
@@ -14,26 +77,27 @@ class Point{
         return this._p2;
     }
 
-    set p1(p1:number){
+    set p1(p1:Point){
         this._p1=p1;
     }
 
-    set p2(p2:number){
+    set p2(p2:Point){
         this._p2=p2;
     }
 
-    public toString(p:Point){
-        return '['+this._p1+';'+this._p2+']' + '['+p.p1+';'+p.p2+']';
+    public toString(){
+        return this._p1.toString() + '-' + this._p2.toString();
     }
 
-    public distance(p:Point){
-        return Math.sqrt((p.p1 - this._p1)**2 + (p.p2 - this._p2)**2);
+    public length(){
+        return this._p1.distance(p2);
     }
-    
 }
 
-const coord1=new Point(1,1);
-const coord2=new Point(9,1);
+let taskas1=new Point(1,1);
+let taskas2=new Point(9,1);
 
-console.log('Taskai yra: ' + coord1.toString(coord2));
-console.log(coord1.distance(coord2));
+let taskai=new Line(taskas1, taskas2);
+
+console.log(taskai.toString());
+console.log(taskai.length());
