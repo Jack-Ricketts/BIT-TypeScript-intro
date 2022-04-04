4de7d8e376fc2af87497612f9be0ecf3eb414333
Jack-Ricketts <zygga.k@gmail.com>
2022-04-04 13:56:49 +0300

    adding bankruptcy and firing option

--- Darbuotojai/app.js
+++ Darbuotojai/app.js
@@ -25,6 +25,7 @@ class Darbuotojas {
     }
 }
 const btnPrideti = document.getElementById("prideti");
+const btnIstrinti = document.getElementById("istrinti");
 const inpVardas = document.getElementById("pavadinimas");
 const inpPavarde = document.getElementById("kaina");
 const inpAlga = document.getElementById("kiekis");
@@ -33,23 +34,34 @@ const gpmSuma = document.getElementById("gpmSuma");
 const psdSuma = document.getElementById("psdSuma");
 const vsdSuma = document.getElementById("vsdSuma");
 let darbuotojai = [];
-darbuotojai.push(new Darbuotojas('Jonas', 'Jonaitis', 1000));
-darbuotojai.push(new Darbuotojas('Tomas', 'Tomaitis', 900));
-darbuotojai.push(new Darbuotojas('Zbygnievas', 'Zbygnievaitis', 399));
-console.log(darbuotojai);
+let jsonString = localStorage.getItem("darbuotojai");
+if (jsonString != null) {
+    let data = JSON.parse(jsonString);
+    data.forEach((obj) => {
+        let empl = new Darbuotojas(obj._vardas, obj._pavarde, obj._atlyginimas);
+        darbuotojai.push(empl);
+    });
+}
 let outputDarbuotojai = () => {
     if (output != null && gpmSuma != null && psdSuma != null && vsdSuma != null) {
         output.innerHTML = "";
         let sumaGPM = 0;
         let sumaPSD = 0;
         let sumaVSD = 0;
-        darbuotojai.forEach((darbininkas) => {
+        darbuotojai.forEach((darbininkas, indeksas) => {
             sumaGPM += darbininkas.atlyginimas * 0.2;
             sumaPSD += darbininkas.atlyginimas * 0.0698;
             sumaVSD += darbininkas.atlyginimas * 0.1252;
             const li = document.createElement("li");
             li.className = "list-group-item";
             li.textContent = 'Vardas: ' + darbininkas.vardas + ", Pavarde: " + darbininkas.pavarde + ", Atlyginimas: " + darbininkas.atlyginimas;
+            const btn = document.createElement("button");
+            btn.textContent = "Atleisti";
+            btn.className = "btn btn-danger float-end";
+            btn.onclick = () => {
+                deleteDarbuotojas(indeksas);
+            };
+            li.appendChild(btn);
             output.appendChild(li);
         });
         let roundedGPM = sumaGPM.toFixed(2);
@@ -60,9 +72,22 @@ let outputDarbuotojai = () => {
         vsdSuma.textContent = Number(roundedVSD) + " EUR";
     }
 };
+let deleteDarbuotojas = (indeksas) => {
+    darbuotojai.splice(indeksas, 1);
+    outputDarbuotojai();
+    localStorage.setItem("darbuotojai", JSON.stringify(darbuotojai));
+};
 if (btnPrideti != null) {
     btnPrideti.onclick = () => {
         darbuotojai.push(new Darbuotojas(inpVardas.value, inpPavarde.value, inpAlga.valueAsNumber));
+        outputDarbuotojai();
+        localStorage.setItem("darbuotojai", JSON.stringify(darbuotojai));
+    };
+}
+if (btnIstrinti != null) {
+    btnIstrinti.onclick = () => {
+        localStorage.removeItem("darbuotojai");
+        darbuotojai = [];
         outputDarbuotojai();
     };
 }
--- Darbuotojai/app.ts
+++ Darbuotojai/app.ts
@@ -33,6 +33,7 @@ class Darbuotojas {
 }
 
 const btnPrideti=document.getElementById("prideti");
+const btnIstrinti=document.getElementById("istrinti");
 
 const inpVardas=<HTMLInputElement>document.getElementById("pavadinimas");
 const inpPavarde=<HTMLInputElement>document.getElementById("kaina");
@@ -44,11 +45,21 @@ const vsdSuma=document.getElementById("vsdSuma");
 
 let darbuotojai:Darbuotojas[]=[];
 
+let jsonString=localStorage.getItem("darbuotojai");
+if (jsonString!=null){
+    let data=JSON.parse(jsonString);
 
-darbuotojai.push(new Darbuotojas('Jonas', 'Jonaitis', 1000));
-darbuotojai.push(new Darbuotojas('Tomas', 'Tomaitis', 900));
-darbuotojai.push(new Darbuotojas('Zbygnievas', 'Zbygnievaitis', 399));
-console.log(darbuotojai);
+    interface dataDarbuotojai{
+        _vardas: string; 
+        _pavarde: string; 
+        _atlyginimas: number; 
+    }
+    
+    data.forEach((obj:dataDarbuotojai) => {
+        let empl=new Darbuotojas(obj._vardas, obj._pavarde, obj._atlyginimas);
+        darbuotojai.push(empl);
+    });
+}
 
 let outputDarbuotojai=()=>{
     if (output!=null && gpmSuma!=null && psdSuma!=null && vsdSuma!=null){
@@ -57,13 +68,23 @@ let outputDarbuotojai=()=>{
         let sumaPSD=0;
         let sumaVSD=0;
         
-        darbuotojai.forEach((darbininkas)=>{
+        darbuotojai.forEach((darbininkas, indeksas)=>{
             sumaGPM+=darbininkas.atlyginimas*0.2;
             sumaPSD+=darbininkas.atlyginimas*0.0698;
             sumaVSD+=darbininkas.atlyginimas*0.1252;
+
             const li=document.createElement("li");
             li.className="list-group-item";
             li.textContent='Vardas: ' + darbininkas.vardas + ", Pavarde: " + darbininkas.pavarde + ", Atlyginimas: "+ darbininkas.atlyginimas;
+            
+            const btn=document.createElement("button");
+            btn.textContent="Atleisti";
+            btn.className="btn btn-danger float-end";
+            btn.onclick=()=>{
+                deleteDarbuotojas(indeksas);
+            };
+            li.appendChild(btn);
+
             output.appendChild(li);
         });
         let roundedGPM = sumaGPM.toFixed(2);
@@ -75,11 +96,26 @@ let outputDarbuotojai=()=>{
     }  
 };
 
+let deleteDarbuotojas=(indeksas:number)=>{
+    darbuotojai.splice(indeksas,1);
+    outputDarbuotojai();
+    localStorage.setItem("darbuotojai",JSON.stringify(darbuotojai));
+}
+
 if (btnPrideti!=null){
     btnPrideti.onclick=()=>{
         darbuotojai.push(new Darbuotojas(inpVardas.value,inpPavarde.value,inpAlga.valueAsNumber));
         outputDarbuotojai();
+        localStorage.setItem("darbuotojai",JSON.stringify(darbuotojai));
     };
+}
+
+if (btnIstrinti!=null){
+    btnIstrinti.onclick=()=>{
+        localStorage.removeItem("darbuotojai");
+        darbuotojai=[];
+        outputDarbuotojai();
+    }
 }
 
 outputDarbuotojai();
